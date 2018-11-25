5cf14e00180ad54eb10be2c5a40df20bfb9b0268
League Student <league@iMac-8.attlocal.net>
2018-11-24 17:27:20 -0800

    Obedient Robot

--- src/methodwriting/ObedientRobot.java
+++ src/methodwriting/ObedientRobot.java
@@ -1,26 +1,52 @@
 package methodwriting;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class ObedientRobot {
-public static void main(String[] args) {
-	Robot rob = new Robot();
-	rob.setSpeed(100);
-	
-	
-
-	
-	
-	rob.penDown();
-for(int i=0; i<4; i++) {
-rob.move(100);
-rob.turn(90);
-}
-	rob.penDown();
-	for(int ii=0; ii< 3; ii++) {
-		rob.move(100);
-		rob.turn(120);
+	 static Robot rob = new Robot();
+	public static void main(String[] args) {
+		String color = JOptionPane.showInputDialog("What color do you want your shape to be (black or white)?");
+		if(color.equals("white")) {
+			rob.setPenColor(255, 255, 255);
+		} else if (color.equals("black")) {
+			rob.setPenColor(0, 0, 0);
+		}
+	String shape = JOptionPane.showInputDialog("What shape do you want to have drawn? Choices: square, triangle, or circle.");
+	if(shape.equals("square")) {
+		drawSquare();
+	} else if(shape.equals("triangle")) {
+		drawTriangle();
+	} else if(shape.equals("circle")) {
+		drawCircle();
 	}
-}
+	
+	
+	}
+	public static void drawSquare() {
+		rob.setSpeed(75);
+		rob.penDown();
+		for(int i = 0; i < 4; i++) {
+			rob.move(200);
+			rob.turn(90);
+		}
+	}
+	public static void drawTriangle() {
+	rob.setSpeed(75);
+		rob.penDown();
+		for(int ii=0; ii<3; ii++) {
+			rob.move(150);
+			rob.turn(120);
+		}
+	}
+	public static void drawCircle() {
+		rob.setSpeed(100);
+		rob.penDown();
+		for(int iii=0; iii < 360; iii++) {
+			rob.move(1);
+			rob.turn(1);
+		}
+	}
 }
 
